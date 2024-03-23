a86f7c938a1162849fbfe8112b9cd9a485693e84
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-22 17:51:33 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# Using Puppet, create a file in /tmp.
+# 0-create_a_file.pp
 
+# Using Puppet, create a file in /tmp.
 file { '/tmp/school':
-	ensure => present,
+	ensure => file,
 	mode => '0744',
 	owner => 'www-data',
 	group => 'www-data',
