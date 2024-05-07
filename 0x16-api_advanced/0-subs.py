df4a2e4af70cf8215c05f5b3d1e0bf4af5f25001
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-05-07 15:27:40 -0700

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,8 @@ def number_of_subscribers(subreddit):
     """ this function return the
     numebr of subscribers on Redit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = request.get(url,
-                           headers={'User-Agent': 'Mozilla/5.0'},
-                           allow_redirects=False)
+    var = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=var, allow_redirects=False)
     if response.status_code == 200:
         return response.json()['data']['subscribers']
     else:
