ca4dd303a8256c87a61e796f03687cb13b9a7b90
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-05-07 17:54:05 -0700

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ this areas is for the file description"""
 import requests
+import re
+
+EXTRANEOUS_WHITESPACE_REGEX = re.compile(r'[\[({] | [)}\],;:]')
 
 
 def number_of_subscribers(subreddit):
@@ -10,6 +13,8 @@ def number_of_subscribers(subreddit):
     var = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=var, allow_redirects=False)
     if response.status_code == 200:
-        return response.json()['data']['subscribers']
+        df = response.json()
+        sub = df['data']['subscribers']
+        return sub
     else:
         return 0
